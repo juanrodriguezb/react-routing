64cad748df0f6dfdcad538408fbe7abc27852f5d
Juan Rodriguez <you@example.com>
2025-01-20 14:57:48 -0500

    Implementing react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
+
+//! Example of components style routing using createRoutesFromElements
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions)
+
+//!
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> }
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> }, // path: ''
+      { path: 'products', element: <ProductsPage />},
+      { path: 'products/:id', element: <ProductDetailPage />}
+    ]
+  },
 ]);
 
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function HomePage() {
-    return <h1>My Home Page</h1>;
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate('products')
+
+    }
+    return (
+    <>
+        <h1>My Home Page</h1>
+        <p>
+            Go to <Link to="products">the List of Products</Link>
+        </p>
+        <p>
+            <button onClick={navigateHandler}>Navigate</button>
+        </p>
+    </>
+    );
 }
 
 export default HomePage;
